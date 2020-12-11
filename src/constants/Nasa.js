801d564c5a98c6a79a0ea65b3d3181f633cfb07c
Lindsey <linds.fonnes@gmail.com>
2020-12-10 20:04:54 -0700

    trying to add styles

--- src/constants/Nasa.js
+++ src/constants/Nasa.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StyledNasa = styled.div`
-
-
-
+const Background = styled.div`
+background-color: black;
 `
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`
+export default function Friend({bold,info,action}){
+    return (
+        <StyledNasa bold={bold} danger={info.}>
+        <Background>
+            <Title>Nasa Photo of The Day!</Title>
+        </Background>
+        </StyledNasa>
+    )
+}
